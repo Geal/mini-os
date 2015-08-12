626b62592a2025ba2fb6215c0e431a31f40a2644
Geoffroy Couprie <geo.couprie@gmail.com>
2015-08-12 14:04:13 +0200

    test

--- rust-mm/src/lib.rs
+++ rust-mm/src/lib.rs
@@ -1,8 +1,30 @@
 #![feature(no_std,lang_items,core,collections)]
 #![no_std]
 
-#[lang="sized"]
-trait Sized {}
+extern crate core;
+extern crate collections;
+
+mod std {
+#[macro_use]
+  pub use core::{fmt, iter, option, ops, slice, mem};
+  pub use collections::{boxed, vec, string};
+  pub mod prelude {
+    pub use core::prelude as v1;
+  }
+}
+
+#[lang = "panic_fmt"] #[inline(never)] #[cold]
+pub extern fn panic_impl(msg: ::core::fmt::Arguments,
+    file: &'static str,
+    line: usize) -> !
+{
+  unsafe {
+    printk(file.as_bytes().as_ptr());
+    //use io::Write;
+    //let _ = write!(terminal::get_terminal(), "{}:{} {}", file, line, msg);
+    ::core::intrinsics::abort();
+  }
+}
 
 #[lang = "stack_exhausted"]
 extern fn stack_exhausted() { /* ... */ }
@@ -10,8 +32,13 @@ extern fn stack_exhausted() { /* ... */ }
 #[lang = "eh_personality"]
 extern fn eh_personality() { /* ... */ }
 
+/*
+#[lang="sized"]
+trait Sized {}
+
 #[lang="copy"]
 trait Copy {}
+*/
 
 extern {
   fn printk(fmt: *const u8, ...);
@@ -20,6 +47,7 @@ extern {
 
 #[no_mangle]
 pub fn fini_mm() {
+  //let prompt = CString::new("HELLO FROM RUST").unwrap();
 }
 
 #[no_mangle]
